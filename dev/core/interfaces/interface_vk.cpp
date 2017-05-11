f49922ac2596fd64329ba5bde4021f1aba851fba
Egor Kurakov <egor@live.ru>
2017-05-11 20:04:17 +0300

    Каркас. Методы интерфейсов

--- dev/core/interfaces/interface_vk.cpp
+++ dev/core/interfaces/interface_vk.cpp
@@ -1,1 +1,26 @@
 #include "interface_vk.h"
+
+InterfaceVK::InterfaceVK(){
+
+};
+
+InterfaceVK::~InterfaceVK(){
+
+};
+
+int InterfaceVK::PrintMessages( std::map<std::string, std::string> data ){
+    //TODO
+    return 0;
+};
+
+int InterfaceVK::PrintWall( std::map<std::string, std::string> data ){
+    //TODO
+    return 0;
+};
+
+int InterfaceVK::PrintFriends( std::map<std::string, std::string> data ){
+    //TODO
+    return 0;
+};
+
+
--- dev/core/interfaces/interfaces.h
+++ dev/core/interfaces/interfaces.h
@@ -1,5 +1,15 @@
 #ifndef interfaces_h
 #define interfaces_h
 
+class Interface() {
+public:
+    Interface();
+    ~Interface();
+
+    int PrintHello();
+    virtual int PrintMessages( std::map<std::string, std::string> data ) = 0;
+    virtual int PrintWall( std::map<std::string, std::string> data ) = 0;
+    virtual int PrintFriends( std::map<std::string, std::string> data ) = 0;
+};
 
 #endif /* interfaces_h */
