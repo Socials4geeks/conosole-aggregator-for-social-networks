788554c282d06fa11d9a75106df71049521dfd6f
Egor Kurakov <egor@live.ru>
2017-05-11 20:06:26 +0300

    Сессии исключительно локальные

--- dev/authorization/authorization.h
+++ dev/authorization/authorization.h
@@ -1,5 +1,14 @@
 #ifndef authorization_h
 #define authorization_h
 
+class Authorizator {
+public:
+    
+    Authorizator();
+    ~Authorizator();
+
+    virtual Session Login( std::string username, std::string password );
+    virtual int Logout( Session session );
+};
 
 #endif /*authorization_h*/
