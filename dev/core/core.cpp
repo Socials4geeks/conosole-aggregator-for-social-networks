ecd79fe7f1624848b6082579d76d599bfcef5a79
Egor Kurakov <egor@live.ru>
2017-05-24 01:13:49 +0300

    Исправил связь Core и Authorizator

--- dev/core/core.cpp
+++ dev/core/core.cpp
@@ -1,11 +1,17 @@
 #include "core/core.h"
 
 Core() {
-
+    std::vector< authInfo > loadedUsers = storage.Pop("LocalLoginsPasswords"); //Use serializator
+    authorizator = new Authorizator(loadedUsers);
 };
 
 ~Core() {
-
+    std::vector< authInfo > loadedOldUsers = storage.Pop("LocalLoginsPasswords"); //Use serializator
+    std::vector< authInfo > gotNewUsers = authorizator->GotUsers();
+    if( gotNewUsers != loadedOldUsers ){
+        storage.Push("LocalLoginsPasswords"); //Use serializator
+    }
+    delete authorizator;
 };
 
 int Core::RemoveSession( Session session ) {
