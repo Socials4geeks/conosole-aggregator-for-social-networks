427b8bfd9d5a31dda08d175366e60ba4aa6c7810
Egor Kurakov <egor@live.ru>
2017-05-24 02:48:52 +0300

    Переписал API_Interface в соответствии с новой концепцией

---
+++ dev/core/implementer.h
@@ -0,0 +1,17 @@
+
+#ifndef implementer_h
+#define implementer_h
+
+class Implementer{
+public:
+
+    Response ExecuteRequest( Request request  ); //Split on basic functional and API
+
+private:
+    
+    std::vector<  >
+
+};
+
+
+#endif /* implementer_h */
--- dev/types.h
+++ dev/types.h
@@ -1,7 +1,19 @@
+#ifndef types_h
+#define types_h
+
 typedef authInfo < std::pair< std::string, std::string > > 
-typedef params < std::map< std::string, std::string > >
+
+typedef params std::map< std::string, std::string >
+
 typedef Request struct {
     std::string IdOfRmoteAccount;
     std::string Action;
     params Params;
 }
+
+typedef status enum {
+    OK,
+    ERROR
+};
+
+#endif /* types_h */
