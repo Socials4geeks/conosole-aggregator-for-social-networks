94875ecb7a3320ec02d1acf7d1fa7ca77da5d76f
Nikita Boyarskikh <boyanik@yandex-team.ru>
2017-05-24 06:34:57 +0300

    Написал методы APIInterface для отправки и приёма сообщений
    Conflicts:
    	dev/core/API/API_interface.h

--- dev/core/API/API_interface.h
+++ dev/core/API/API_interface.h
@@ -2,41 +2,30 @@
 #define API_Interface_h
 
 #include <map>
+#include <string>
 
 #include "core/API/APIs.h"
 
-class APIInterface : API {
+class APIInterface {
 public:
-    APIInterface() : api();
-    /// Залогинивает пользователя с login, password
-    status Login( std::string login, std::string password ) = 0;
-    /// Разлогинивает текущего пользователя
-    status Logout() = 0;
-    /// Отправляет сообщение от текущего пользователя, если авторизован
-    status SendMessage( std::map<std::string, std::string> params ) = 0;
-    /// Показывает n последних сообщений от пользователя user
-    status ShowMessages( std::map<std::string, std::string> params ) = 0;
-    /// TODO
-    status AddFriend( std::map<std::string, std::string> params ) = 0;
-    /// TODO
-    status GetFriends( std::map<std::string, std::string> params ) = 0;
-    /// TODO
-    status DeleteFriend( std::map<std::string, std::string> params ) = 0;
-    /// TODO
-    status GetWall( std::map<std::string, std::string> params ) = 0;
-    /// TODO
-    status PostWall( std::map<std::string, std::string> params ) = 0;
-    /// TODO
-    status ( std::map<std::string, std::string> params ) = 0;
-    // TODO
+    APIInterface();
 
 protected:
-    /// Статусы возврата функций
+    virtual status SendMessage( authInfo loginPassword,  std::string topic, std::string text, std::string recipient, response& Response );  // TODO: recipients => recipients
+    /// Показывает n последних сообщений от пользователя user
+    virtual status ShowMessages( authInfo loginPassword, response& Response );
+    virtual status AddFriend(authInfo loginPassword, std::string idOfFriend, response& Response );
+    virtual status GetFriends(authInfo loginPassword, response& Response );
+    virtual status RemoveFriend(authInfo loginPassword, std::string idOfFriend, response& Response );
+    virtual status GetWall( authInfo loginPassword, response& Response );
+    virtual status AddWall( authInfo loginPassword, std::string text  );  // TODO: Replace std::string text to special struct for each SN
+
+protected:
     API api;
-    enum status {
-        OK,
-        ERROR,
-    };
+
+private:
+    void login_if_not_logined( authInfo loginPassword );
+    status checkCorrectLoginPassword( authInfo loginPassword, response& Response, int& uid );
 };
 
 
