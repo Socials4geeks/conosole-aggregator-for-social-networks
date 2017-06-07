29eef51f9a260a93237b7774db6aa441b94a5f73
Praron <koctr123@mail.ru>
2017-06-07 19:29:26 +0300

    Удалил из коммита генерируемые папки.

--- include/API.h
+++
@@ -1,2 +0,0 @@
-#include "core/API/API_telegram.h"
-#include "core/API/API_vk.h"
--- include/API_telegram.h
+++
@@ -1,6 +0,0 @@
-#ifndef API_telegram_h
-#define API_telegram_h
-
-#include "core/API/APIs.h"
-
-#endif /* API_telegram_h */
