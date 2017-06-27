214642cca6d833fcf2dc810ab0e4d5304c2f40ae
Egor Kurakov <egor@live.ru>
2017-06-27 20:51:22 +0300

    Добавил возможность добавления и удаления логипаролей для социалок

--- dev/core/session.cpp
+++ dev/core/session.cpp
@@ -1,7 +1,8 @@
 #include "session.h"
+#include "exceptions.h"
 
 Session::Session(): EmptySession( true ) {
-    //TODO
+    
 };
 
 Session::Session( std::string login ) : 
@@ -14,13 +15,34 @@ Session::Session( std::string login, params settings ) :
     user_settings( settings ) {};
 
 authInfo Session::GetAuthForApi( std::string nameOfRemoteAccount ) {
-    return authsForSocialNetworks[ "nameOfRemoteAccount" ];
+    if( authsForSocialNetworks.find( nameOfRemoteAccount ) == authsForSocialNetworks.end() ) {
+        throw NoAuthData();
+    }
+    return authsForSocialNetworks[ nameOfRemoteAccount ];
+}
+
+Status Session::AddAuthForApi( std::string nameOfRemoteAccount, authInfo loginPassword ) {
+    authsForSocialNetworks[ nameOfRemoteAccount ] = loginPassword;
+    return OK;
+}
+
+Status Session::DeleteAuthForApi( std::string nameOfRemoteAccount ) {
+    auto i = authsForSocialNetworks.find( nameOfRemoteAccount );
+    if( i == authsForSocialNetworks.end() ) {
+            authsForSocialNetworks.erase( i );
+            return OK;
+    }
+    return UNKNOWN_ERROR;
 }
 
 bool Session::operator==( Session session ) {
     return localLogin == session.localLogin;
 }
 
+bool Session::IsEmpty() {
+    return EmptySession;
+}
+
 Session::~Session() {
-    // TODO: Save changes authInfo for social networks and local too
+    
 }
